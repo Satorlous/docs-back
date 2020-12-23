fdf99ebf53ba77d26eef37ff9c493b2fc36970d1
Satorlous <n.leppik@gmail.com>
2020-12-24 01:15:04 +0500

    Initial

---
+++ src/main/java/com/utmn/bulls/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.utmn.bulls.controller;
+
+import com.utmn.bulls.exception.ResourceNotFoundException;
+import com.utmn.bulls.model.User;
+import com.utmn.bulls.repository.UserRepository;
+import com.utmn.bulls.security.CurrentUser;
+import com.utmn.bulls.security.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+        return userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    }
+}
---
+++ src/main/java/com/utmn/bulls/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.utmn.bulls.repository;
+
+import com.utmn.bulls.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+}
